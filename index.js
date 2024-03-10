d3bc1d30c5d10e198dd27805d1cd23982d224a10
shivansh aggarwal <shivneeraj2004@gmail.com>
2024-03-10 13:14:31 +0530

    made my website responsive

---
+++ index.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector(".nav2")
+const navigation = document.querySelector(".navigation")
+
+navigation.addEventListener("click", () => {
+    nav.classList.toggle("hide")
+})
+
